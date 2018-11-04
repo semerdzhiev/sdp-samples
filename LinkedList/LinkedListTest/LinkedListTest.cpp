265e70e70839a0a5d845447534072704072bb21f
Atanas Semerdzhiev <semerdjiev@live.com>
2018-11-04 16:52:27 +0200

    Further improvements to the unt tests in LinkedList

--- LinkedList/LinkedListTest/LinkedListTest.cpp
+++ LinkedList/LinkedListTest/LinkedListTest.cpp
@@ -16,6 +16,84 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace LinkedListTest
 {
+    class ListUtils
+    {
+    public:
+        ///
+        /// Checks whether list contains all integers in [First,Last].
+        ///
+        /// The integers should be stored in ascending order in the list.
+        ///
+        static bool ContainsRange(const LinkedList<int>& List, int First, int Last)
+        {
+            LinkedList<int>::Iterator it = List.GetIterator();
+
+            int i = First;
+
+            while (!it.EndReached() && i <= Last)
+            {
+                if (it.GetCurrent() != i)
+                    break;
+
+                i++;
+                it.MoveNext();
+            }
+
+            return i > Last;
+        }
+
+        ///
+        /// Fills a list with all integers in the interval [Start, End].
+        ///
+        static void Fill(LinkedList<int>& List, int First, int Last)
+        {
+            for (int i = First; i <= Last; i++)
+                List.PushBack(i);
+        }
+
+        static void AssertEmpty(const LinkedList<int>& List)
+        {
+            Assert::IsTrue(List.IsEmpty());
+            Assert::AreEqual(List.GetSize(), size_t(0));
+        }
+
+        static void AssertSize(const LinkedList<int>& List, size_t Size)
+        {
+            Assert::AreEqual(List.GetSize(), Size);
+        }
+
+        static void AssertContainsRange(const LinkedList<int>& List, int First, int Last)
+        {
+            Assert::IsTrue(ContainsRange(List, First, Last));
+        }
+    };
+
+    class Range
+    {
+    public:
+        const int First;
+        const int Last;
+        const int Size;
+
+        Range(int First, int Last)
+            : First(First), Last(Last), Size(Last - First + 1)
+        {
+        }
+
+        void Fill(LinkedList<int>& List)
+        {
+            ListUtils::Fill(List, First, Last);
+        }
+
+        void AssertOn(LinkedList<int>& List)
+        {
+            ListUtils::AssertContainsRange(List, First, Last);
+            ListUtils::AssertSize(List, Size);
+        }
+    };
+
+    //
+    // Tests the behavior of operations on one or more empty lists.
     //
     // Implicitly, the class checks that the default constructed list is empty
     // (IsEmpty_ReturnsTrue + GetSize_ReturnsZero on the EmptyList object)
@@ -76,6 +154,29 @@ namespace LinkedListTest
                 [this]() { EmptyList.Back(); } // Retrieve the rearmost element
             );
         }
+
+        TEST_METHOD(CopyConstructor_OnEmptyLists)
+        {
+            LinkedList<int> test(EmptyList);
+
+            ListUtils::AssertEmpty(test);
+        }
+
+        TEST_METHOD(Assignment_BetweenEmptyLists)
+        {
+            LinkedList<int> test;
+            test = EmptyList;
+
+            ListUtils::AssertEmpty(test);
+        }
+
+        TEST_METHOD(Append_OnEmptyLists)
+        {
+            LinkedList<int> test;
+            test.Append(EmptyList);
+
+            ListUtils::AssertEmpty(test);
+        }
     };
 
     TEST_CLASS(ListTest)
@@ -83,20 +184,21 @@ namespace LinkedListTest
     private:
         LinkedList<int> EmptyList;
         LinkedList<int> SampleList;
-        const int SampleRangeStart;
-        const int SampleRangeEnd;
-        const size_t SampleSize;
+        Range SampleRange;
 
     public:
         ListTest()
-            : SampleRangeStart(1),
-              SampleRangeEnd(10),
-              SampleSize(SampleRangeEnd - SampleRangeStart + 1)
+            : SampleRange(1, 10)
         {
-            Logger::WriteMessage(L"ListTest::ListTest()");
-            Fill(SampleList, SampleRangeStart, SampleRangeEnd);
+            // Uncomment the line below to see the order in which
+            // the unit test framework invokes each method
+            // Logger::WriteMessage(L"ListTest::ListTest()");
+            SampleRange.Fill(SampleList);
         }
 
+        // Uncomment the code below to see the order in which
+        // the unit test framework invokes each method
+        /*
         ~ListTest()
         {
             Logger::WriteMessage(L"ListTest::~ListTest()");
@@ -121,133 +223,31 @@ namespace LinkedListTest
         {
             Logger::WriteMessage(L"TestClassCleanup");
         }
-        
-        ///
-        /// Checks whether list contains all integers in [start,end].
-        ///
-        /// The integers should be stored in ascending order in the list.
-        ///
-        bool ContainsRange(const LinkedList<int>& list, int start, int end)
-        {
-            LinkedList<int>::Iterator it = list.GetIterator();
-
-            int i = start;
-
-            while (!it.EndReached() && i <= end)
-            {
-                if (it.GetCurrent() != i)
-                    break;
-
-                i++;
-                it.MoveNext();
-            }
-
-            return i > end;
-        }
-
-
-        ///
-        ///    Fills a list with all integers in the interval [Start, End].
-        ///
-        void Fill(LinkedList<int>& list, int Start, int End)
-        {
-            for (int i = Start; i <= End; i++)
-                list.PushBack(i);
-        }
-
-
-        ///
-        /// Asserts that the list is empty
-        ///
-        void AssertEmpty(const LinkedList<int>& list)
-        {
-            Assert::IsTrue(list.IsEmpty());
-            Assert::AreEqual(list.GetSize(), size_t(0));
-        }
-
-        void AssertContainsRange(const LinkedList<int>& list, int start, int end)
-        {
-            Assert::IsTrue(ContainsRange(list, start, end));
-        }
-
-        void AssertContainsSampleRange(const LinkedList<int>& list)
-        {
-            Assert::IsTrue(ContainsRange(list, SampleRangeStart, SampleRangeEnd));
-        }
-
-        void AssertSize(const LinkedList<int>& list, size_t size)
-        {
-            Assert::AreEqual(list.GetSize(), size);
-        }
-
-
+        */
 
         TEST_METHOD(SelfCheck)
         {
-            Assert::IsTrue(ContainsRange(SampleList, 1, 10));
+            SampleRange.AssertOn(SampleList);
         }
 
         TEST_METHOD(Clear_EmptiesTheList)
         {
             SampleList.Clear();
-            AssertEmpty(SampleList);
-        }
 
-        TEST_METHOD(CopyConstructor_CopiesCorrectly)
-        {
-            LinkedList<int> copy(SampleList);
-            AssertContainsSampleRange(copy);
-        }
-
-        TEST_METHOD(CopyConstructor_WorksForEmptyLists)
-        {
-            LinkedList<int> copy(EmptyList);
-            AssertEmpty(copy);
-        }
-
-        TEST_METHOD(Append_CopiesCorrectly)
-        {
-            LinkedList<int> anotherList;
-            Fill(anotherList, SampleRangeEnd + 1, SampleRangeEnd + 10);
-            SampleList.Append(anotherList);
-
-            AssertSize(SampleList, SampleSize + 10);
-            AssertContainsRange(SampleList, SampleRangeStart, SampleRangeEnd + 10);
-        }
-
-        TEST_METHOD(Append_AppendingAnEmptyListShouldPreserveTheOriginal)
-        {
-            SampleList.Append(EmptyList);            
-            
-            AssertSize(SampleList, SampleSize);
-            AssertContainsRange(SampleList, SampleRangeStart, SampleRangeEnd);
-        }
-
-        TEST_METHOD(Append_AppendingToAnEmptyListShouldCopyCorrectly)
-        {
-            EmptyList.Append(SampleList);
-            
-            AssertContainsSampleRange(EmptyList);
-        }
-
-        TEST_METHOD(Append_AppendingEmptyToEmptyShouldWork)
-        {
-            EmptyList.Append(EmptyList);
-            AssertEmpty(EmptyList);
+            ListUtils::AssertEmpty(SampleList);
         }
 
         TEST_METHOD(Head_CanActuallyModifyTheHead)
         {
-            int newValue = SampleRangeStart - 1; // A value, which is not in the list
+            int newValue = SampleRange.First - 1; // A value, which is not in the list
 
             SampleList.Front() = newValue;
             Assert::AreEqual(SampleList.Front(), newValue);
 
-            SampleList.Front() = SampleRangeStart;
-            Assert::AreEqual(SampleList.Front(), SampleRangeStart);
+            SampleList.Front() = SampleRange.First;
+            Assert::AreEqual(SampleList.Front(), SampleRange.First);
 
-            AssertSize(SampleList, SampleSize); // make sure the operation does not resize the list
-            AssertContainsSampleRange(SampleList); // make sure the operation does not work on a memory of its own
+            SampleRange.AssertOn(SampleList); // The list should be restored to its initial state
         }
 
         TEST_METHOD(HeadOperationsWorkCorrectly)
@@ -261,44 +261,42 @@ namespace LinkedListTest
             // 3. The head can be retrieved
             // 4. The value of the head matches the value added
             //
-            for (int i = SampleSize - 1; i >= 0; --i)
+            for (int i = SampleRange.Last; i >= SampleRange.First; --i)
             {
-                list.PushFront(SampleRangeStart + i);
+                list.PushFront(i);
 
-                Assert::AreEqual(list.Front(), SampleRangeStart + i);
-                AssertSize(list, SampleSize - i);
+                Assert::AreEqual(list.Front(), i);
+                ListUtils::AssertSize(list, SampleRange.Last - i + 1);
                 Assert::IsFalse(list.IsEmpty());
             }
 
             // Make sure that the elements are there
-            AssertContainsSampleRange(list);
-            AssertSize(list, SampleSize);
+            SampleRange.AssertOn(list);
 
             // Now remove all items, one by one, from the front
-            for (int i = 0; i < SampleSize; ++i)
+            for (int i = SampleRange.First; i <= SampleRange.Last; ++i)
             {
-                Assert::AreEqual(list.Front(), SampleRangeStart + i);
-                AssertSize(list, SampleSize - i);
+                Assert::AreEqual(list.Front(), i);
+                ListUtils::AssertSize(list, SampleRange.Last - i + 1);
                 Assert::IsFalse(list.IsEmpty());
 
                 list.PopFront();
             }
 
-            AssertEmpty(list);
+            ListUtils::AssertEmpty(list);
         }
 
         TEST_METHOD(Tail_CanActuallyModifyTheTail)
         {
-            int newValue = SampleRangeEnd + 1; // A value, which is not in the list
+            int newValue = SampleRange.Last + 1; // A value, which is not in the list
 
             SampleList.Back() = newValue;
             Assert::AreEqual(SampleList.Back(), newValue);
 
-            SampleList.Back() = SampleRangeEnd;
-            Assert::AreEqual(SampleList.Back(), SampleRangeEnd);
+            SampleList.Back() = SampleRange.Last;
+            Assert::AreEqual(SampleList.Back(), SampleRange.Last);
 
-            AssertSize(SampleList, SampleSize); // make sure the operation does not resize the list
-            AssertContainsSampleRange(SampleList); // make sure the operation does not work on a memory of its own
+            SampleRange.AssertOn(SampleList); // The list should be restored to its initial state
         }
 
 
@@ -316,96 +314,146 @@ namespace LinkedListTest
             // 3. The back of the list can be retrieved
             // 4. The value of the back matches the value added
             //
-            for (int i = 0; i < SampleSize; ++i)
+            for (int i = SampleRange.First; i <= SampleRange.Last; ++i)
             {
-                list.PushBack(SampleRangeStart + i);
+                list.PushBack(i);
 
-                Assert::AreEqual(list.Back(), SampleRangeStart + i);
-                AssertSize(list, i + 1);
+                Assert::AreEqual(list.Back(), i);
+                ListUtils::AssertSize(list, i - SampleRange.First + 1);
                 Assert::IsFalse(list.IsEmpty());
             }
 
             // Make sure that the elements are there
-            AssertContainsSampleRange(list);
-            AssertSize(list, SampleSize);
+            SampleRange.AssertOn(list);
 
             // Now remove all items, one by one, from the back
-            for (int i = SampleSize - 1; i >= 0; --i)
+            for (int i = SampleRange.Last; i >= SampleRange.First; --i)
             {
-                Assert::AreEqual(list.Back(), SampleRangeStart + i);
-                AssertSize(list, i + 1);
+                Assert::AreEqual(list.Back(), i);
+                ListUtils::AssertSize(list, i - SampleRange.First + 1);
                 Assert::IsFalse(list.IsEmpty());
 
                 list.PopBack();
             }
 
-            AssertEmpty(list);
+            ListUtils::AssertEmpty(list);
         }
 
-        ///
-        ///    Test at index operations
-        ///
         TEST_METHOD(At_ReturnsCorrectValues)
         {
-            const int TestSize = 5;
+            // We work with a small value here, because the loop has quadratic complexity
+            const int MaxValue = 5;
 
             LinkedList<int> list;
 
-            Fill(list, 0, TestSize);
+            ListUtils::Fill(list, 0, MaxValue);
 
-            for (int i = 0; i <= TestSize; ++i)
+            for (int i = 0; i <= MaxValue; ++i)
                 Assert::AreEqual(list.At(i), i);
         }
 
         TEST_METHOD(At_AtInvalidIndexThrows)
         {
             Assert::ExpectException<std::out_of_range>(
-                    [this]() { SampleList.At(SampleSize + 1); }
-                );
+                [this]() { SampleList.At(SampleRange.Size + 1); }
+            );
+        }
+
+        TEST_METHOD(CopyConstructor_CopiesCorrectly)
+        {
+            LinkedList<int> copy(SampleList);
+            SampleRange.AssertOn(SampleList);
+        }
+
+        TEST_METHOD(AssignmentOperator_CreatesExactCopy)
+        {
+            LinkedList<int> list;
+            Range range(20, 30);
+
+            range.Fill(list);
+            range.AssertOn(list);
+
+            list = SampleList;
+
+            SampleRange.AssertOn(list);
+        }
+
+        TEST_METHOD(AssignmentOperator_AssigningFromEmptyListErasesAllContent)
+        {
+            SampleList = EmptyList;
+            ListUtils::AssertEmpty(SampleList);
+        }
+
+        TEST_METHOD(Append_TheEmptyListIsLeftIdentity)
+        {
+            // From the algebraic theory: for a given set S,
+            // an element E in S is a left identity with respect to the operation *,
+            // iff E * A = A, for all A in S
+
+            EmptyList.Append(SampleList);
+            SampleRange.AssertOn(EmptyList);
+        }
+
+        TEST_METHOD(Append_TheEmptyListIsRightIdentity)
+        {
+            SampleList.Append(EmptyList);
+            SampleRange.AssertOn(SampleList);
+        }
+
+        TEST_METHOD(Append_CopiesCorrectly)
+        {
+            Range secondRange(SampleRange.Last + 1, SampleRange.Last + 10);
+            LinkedList<int> secondList;
+
+            secondRange.Fill(secondList);
+
+            SampleList.Append(secondList);
+
+            Range combinedRange(SampleRange.First, secondRange.Last);
+            combinedRange.AssertOn(SampleList);
         }
 
         ///
         ///    Test iterator functionality
         ///
-        TEST_METHOD(Iterator_CanCorrectlyReadTheList)
+        TEST_METHOD(Iterator_CanCorrectlyIterate)
         {
             LinkedList<int>::Iterator it = SampleList.GetIterator();
 
-            for (size_t i = 0; i < SampleSize; i++)
+            for (int i = SampleRange.First; i <= SampleRange.Last; ++i)
             {
                 Assert::IsFalse(it.EndReached());
-                Assert::AreEqual(it.GetCurrent(), SampleRangeStart + static_cast<int>(i));
+                Assert::AreEqual(it.GetCurrent(), i);
                 it.MoveNext();
             }
 
             // Assert that the iterator is at its end
             Assert::IsTrue(it.EndReached());
+        }
 
-            // Now rewind and try again
+        TEST_METHOD(Iterator_CanRewind)
+        {
+            LinkedList<int>::Iterator it = SampleList.GetIterator();
+
+            while (!it.EndReached())
+                it.MoveNext();
+
             it.Rewind();
 
-            for (size_t i = 0; i < SampleSize; i++)
-            {
-                Assert::IsFalse(it.EndReached());
-                Assert::AreEqual(it.GetCurrent(), SampleRangeStart + static_cast<int>(i));
-                it.MoveNext();
-            }
-
-            // Assert that the iterator is at its end
-            Assert::IsTrue(it.EndReached());
-
-            // The list should remain the same
-            AssertContainsSampleRange(SampleList);
+            Assert::AreEqual(it.GetCurrent(), SampleRange.First); // Must be at the start of list now
         }
 
         TEST_METHOD(Iterator_CanModifyTheList)
         {
+            const int shiftBy = 100;
+            Range shiftedRange(SampleRange.First + shiftBy, SampleRange.Last + shiftBy);
+
             LinkedList<int>::Iterator it = SampleList.GetIterator();
 
-            for (it.Rewind(); !it.EndReached(); it.MoveNext())
-                it.SetCurrent(it.GetCurrent() + 100);
+            for (; !it.EndReached(); it.MoveNext())
+                it.SetCurrent(it.GetCurrent() + shiftBy);
 
-            AssertContainsRange(SampleList, SampleRangeStart + 100, SampleRangeEnd + 100);
+            shiftedRange.AssertOn(SampleList);
         }
     };
 }
